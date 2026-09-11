9f07efbdfad8782e4e6446b8ea576a08d522ec72
宮森　優菜 <3324yuna@gmail.com>
2026-09-11 23:17:28 +0900

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,76 @@
+// ========================================
+// 設定ページ
+// ========================================
 
+console.log("settings.js 読み込み成功");
+// HTMLの読み込みが終わったら実行
+
+document.addEventListener(
+  "DOMContentLoaded",
+  async () => {
+
+    // 保存されているユーザー情報を取得
+    const user = await getUser();
+
+
+    // データが存在する場合
+    if (user) {
+
+      document.getElementById("nameInput").value =
+        user.name || "";
+
+      document.getElementById("gradeInput").value =
+        user.grade || "";
+
+    }
+
+  }
+);
+
+
+// ========================================
+// 保存ボタン
+// ========================================
+
+document
+  .getElementById("saveButton")
+  .addEventListener("click", async () => {
+
+    console.log("保存ボタンが押されました");
+
+    // 入力された名前
+    const name =
+      document.getElementById("nameInput").value;
+
+
+    // 選択された学年
+    const grade =
+      document.getElementById("gradeInput").value;
+
+
+    // ユーザー情報を作成
+
+    const user = {
+
+      id: "current",
+
+      name: name,
+
+      grade: grade
+
+    };
+
+
+    // データベースに保存
+
+    await saveUser(user);
+
+
+    // 保存完了メッセージ
+
+    document.getElementById(
+      "saveMessage"
+    ).textContent =
+      "保存しました！";
+
+  });
